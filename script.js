b1c614d24268e901dd6216b9d98beda423ccd56f
OceaneEmile <oceane.emile@oclock.school>
2024-03-26 18:52:18 +0100

    Init project +prompt

---
+++ script.js
@@ -0,0 +1,22 @@
+/* 
+États de notre Tamastudi possibles :
+- 🥚 : partie non lancée
+- 🐣 : naissance pendant tant qu'il n'a pas fait son 1er caca
+Ensuite il devient un "grand" avec une humeur variable
+- 😢 : triste 0/5
+- 🙁 : pas content 1/5
+- 🙂 : normal 2/5
+- 😄 : content 3/5
+- 🤗 : heureux 4/5
+- 🥰 : très heureux 5/5
+- 👻 : mort 0/5
+Ses envies :
+- 😋 : faim, aléatoire minimum 30 sec et max 3 minutes
+- 🥱 : jouer, aléatoire minimum 30 sec et max 3 minutes
+- 💩 : caca, aléatoire minimum 30 sec et max 1.30 minutes uniquement après avoir mangé
+*/
+
+/* Demander le prénom */
+
+const tamaName = prompt ("Quel est ton nom?");
+console.log(tamaName);
