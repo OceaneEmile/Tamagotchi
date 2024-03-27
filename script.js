0ae76d80f121ad5d90e87ddf47fd4262081882e7
OceaneEmile <oceane.emile@oclock.school>
2024-03-27 10:26:43 +0100

    Creation fonction

--- script.js
+++ script.js
@@ -16,33 +16,29 @@ Ses envies :
 - 💩 : caca, aléatoire minimum 30 sec et max 1.30 minutes uniquement après avoir mangé
 */
 
-/* Demander le prénom */
+// Creation d'une fonction
+const startTama = () => {
+
+
+/* 1. Demander le prénom */
 const tamaName = prompt ("Quel est ton nom?");
 //console.log(tamaName);
-
-/* Faire eclore mon oeuf pour passer au poussin */
+/* 2. Faire eclore mon oeuf pour passer au poussin */
 const character = document.querySelector(".js-character");
-
-/*character a plusieurs propriétés dt text content pour changer la variable, je remplace le contenu"
-*/
+//character a plusieurs propriétés dt text content pour changer la variable, je remplace le contenu"
 character.textContent="🐣";
 //console.log(character);
-
-
-/* Afficher mes vitals,  je dois retirer hidden du html dom , j'ajoute classe js dans html */
+/* 3. Afficher mes vitals,  je dois retirer hidden du html dom , j'ajoute classe js dans html */
 const vitals = document.querySelector(".js-vitals");
 //console.log(vitals);
 vitals.classList.remove("hidden");
 // ici , je ne mets pas de . devant hidden car dans classlist , javascript sait deja que c'est une classe contrairement au queryselector
-
-//Afficher le nom de mon Tama dans les vitals
+//4. Afficher le nom de mon Tama dans les vitals
 // Sélectionne l'élément HTML ayant la classe "js-tamaName" et le stocke dans la variable nameDisplay
 const nameDisplay = document.querySelector(".js-tamaName");
 // Modifie le texte contenu dans l'élément nameDisplay avec la valeur de la variable tamaName ligne 20
 // il faut decommenter console.log de TamanName ligne 20 pour voir si bien le resultat du prompt
 nameDisplay.textContent = tamaName;
-
-
 //5. Mettre les scores des vitals à 5
 // je pourrai selectionner tous les scores chacun, mais je vais modifier les classes pour que js-score devient commun
 //const eatScoreDisplay = document.querySelector
@@ -53,5 +49,7 @@ console.log(scoresDisplay);// ca m'affiche bien 0
 // la bocule itère chaque element 
 scoresDisplay.forEach((score) =>{
 //console.log(score);
-score.textContent = 5;
+    score.textContent = 5;
 });
+};
+
